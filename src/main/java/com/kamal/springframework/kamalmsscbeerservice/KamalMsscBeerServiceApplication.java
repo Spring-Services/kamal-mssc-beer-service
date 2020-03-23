38fd277de1ddb43ccad3119e4b43822f30a8bc8e
Kamaldwip Choudhury (V) <kamaldwip_choudhury@apple.com>
2020-03-22 21:29:43 -0500

    Initial project add. Closes #1

---
+++ src/main/java/com/kamal/springframework/kamalmsscbeerservice/KamalMsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kamal.springframework.kamalmsscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamalMsscBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamalMsscBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kamal/springframework/kamalmsscbeerservice/KamalMsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamal.springframework.kamalmsscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamalMsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
